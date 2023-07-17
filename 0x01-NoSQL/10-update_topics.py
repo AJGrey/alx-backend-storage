744f7b70f88f991cb8ebfa63d96fcb53155e2a0c
AJGrey <greyajayi@yahoo.com>
2023-07-17 22:25:01 +0000

    update_many

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/ env python3
+""" A python function that changes the topics of all documents based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of all documents"""
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+
